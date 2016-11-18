18356cd8886bb11d335dac5f70c001a5ca9c2b6a
Christina Azodi <azodichr@msu.edu>
2016-11-18 10:06:58 -0500

    Create data_management.py

---
+++ data_management.py
@@ -0,0 +1,20 @@
+import sys
+from sklearn.cross_validation import train_test_split
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+for i in range (1,len(sys.argv),2):
+      if sys.argv[i] == "-geno":
+        G = sys.argv[i+1]
+      if sys.argv[i] == "-pheno":
+        P = sys.argv[i+1] 
+
+g <- pd.read_csv(G)
+p <- pd.read_csv(P)
+
+print(g.head(3))
+print(p.head(4))
+
+
