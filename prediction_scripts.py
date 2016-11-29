8068520f0dbac908c3ae306ae634eb91c3c39d1c
ChristinaAzodi <azodichr@msu.edu>
2016-11-29 10:48:52 -0500

    RF v.1.0 done - bad prediction and plotting won't work on HPC

--- prediction_scripts.py
+++ prediction_scripts.py
@@ -16,27 +16,29 @@ OUTPUT:
 
 import sys
 from sklearn.model_selection import KFold
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, r2_score
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-      
+
 class prediction:
 
       def RF(X, Y, cv):
             """ Predict trait using Random Forest """
             
             numberTrees = 100
-            mse = []
+            
             Y_TRUE = []
             Y_PRED = []
+            mse = []
+            r2 = []
 
             for train_index, test_index in kf.split(X):
               #print("TRAIN:", train_index, "TEST:", test_index)
               X_train, X_test = X[train_index], X[test_index]
               y_train, y_test = Y[train_index], Y[test_index]
               
-              model = RandomForestRegressor(n_estimators=numberTrees, max_features="sqrt")
+              model = RandomForestRegressor(n_estimators=numberTrees)  # max_features="sqrt"
               model.fit(X_train, y_train)
 
               y_pred = model.predict(X_test)
@@ -44,16 +46,10 @@ class prediction:
               Y_TRUE = np.append(Y_TRUE, y_test)
               Y_PRED = np.append(Y_PRED, y_pred)
               mse = np.append(mse, (mean_squared_error(y_test, y_pred)))
+              r2 = np.append(r2, (r2_score(y_test, y_pred)))
 
-            Y_TRUE = np.array(Y_TRUE)
-            Y_PRED = np.array(Y_PRED)
-            mse = np.array(mse)
-            print("KF MSE: %0.2f (+/- %0.2f)" % (mse.mean(), mse.std()*2))
-
-            plt.scatter(Y_TRUE, Y_PRED)
-            plt.xlabel('True Trait Value')
-            plt.ylabel('Predicted Trait Value')
-            plt.show()
+            return Y_TRUE, Y_PRED, mse, r2
+            
 
 
 
@@ -106,7 +102,7 @@ if __name__ == "__main__":
       
       if M == "RF" or M == "RandomForest":
             from sklearn.ensemble import RandomForestRegressor
-            prediction.RF(X, Y, kf)
+            Y_TRUE, Y_PRED, mse, r2 = prediction.RF(X, Y, kf)
       
       elif M == "NN" or M == "NeuralNetworks":
             prediction.NN(X, Y, kf)
@@ -114,6 +110,19 @@ if __name__ == "__main__":
             print("Prediction method not available in this script")
           
       
-       ### Score Predictions ###
+      ### Score Predictions ###
           
-      
+      #for i in range(1, len(Y_TRUE)):
+      #        print(Y_TRUE[i], Y_PRED[i])
+
+      print("KF MSE: %0.2f (+/- %0.2f)" % (mse.mean(), mse.std()*2))
+      print("KF R^2: %0.2f (+/- %0.2f)" % (r2.mean(), r2.std()*2))
+
+      fig, ax = plt.subplots()
+
+      ax.scatter(Y_TRUE, Y_PRED)
+      ax.xlabel('True Trait Value')
+      ax.ylabel('Predicted Trait Value')
+
+      plt.show()
+  
